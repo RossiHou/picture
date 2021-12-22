2a1c60da23a9adafc2aa8829c35b9b0634dc0116
rossihou <981273597@qq.com>
2021-12-22 23:11:11 +0800

    2021.12.22

--- picture.py
+++ picture.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
-x_value = [1,2,3,4,5]
-y_value = [1,4,9,12,25]
-plt.plot(x_value,y_value,linewidth=5)
-plt.title("squres number", fontsize=24)
-plt.xlabel("value", fontsize = 24)
-plt.ylabel("squre of value", fontsize= 24)
-plt.tick_params(axis='both',which = 'major',labelsize=14)
+x_values = list(range(1,1001))
+y_values = [x**2 for x in x_values]
+
+plt.scatter(x_values, y_values, c=y_values,cmap=plt.cm.Blues,edgecolor='none', s=40)
+plt.axis([0,1100,0,1100000])
+plt.title("squre numbers", fontsize=24)
+plt.xlabel("value" , fontsize = 14)
+plt.ylabel("squre of value" , fontsize = 14)
+
 plt.show()
