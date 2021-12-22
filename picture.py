1d945f4e87db4ac681fa058e5a360726b5afd464
rossihou <981273597@qq.com>
2021-12-22 22:17:23 +0800

    第一次修改

--- picture.py
+++ picture.py
@@ -1,4 +1,9 @@
 import matplotlib.pyplot as plt
-squres = [1,4,9,16,25]
-plt.plot(squres)
+x_value = [1,2,3,4,5]
+y_value = [1,4,9,12,25]
+plt.plot(x_value,y_value,linewidth=5)
+plt.title("squres number", fontsize=24)
+plt.xlabel("value", fontsize = 24)
+plt.ylabel("squre of value", fontsize= 24)
+plt.tick_params(axis='both', labelsize=14)
 plt.show()
